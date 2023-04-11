0e631dbf0beaf304532de9a299008993609a1849
fisjac <jfisher9882@gmail.com>
2023-04-11 11:52:47 -0500

    min subarray solved

--- arrays/Min_Subarray_Sum.py
+++ arrays/Min_Subarray_Sum.py
@@ -1,1 +1,16 @@
 # https://leetcode.com/problems/minimum-size-subarray-sum/
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        cur_sum = sum(nums)
+
+        min_len = 0
+        if cur_sum < target:
+            return 0
+        else:
+            nums.sort(reverse = True)
+            while target <= cur_sum:
+                cur_sum -= nums.pop()
+                print(cur_sum)
+            return len(nums) + 1
