16cbab5ad5b69729a9f6fc6099487364838dcec9
fisjac <jfisher9882@gmail.com>
2023-05-09 12:16:39 -0500

    len of last word

--- strings/int_to_roman.py
+++ strings/int_to_roman.py
@@ -10,15 +10,13 @@ class Solution:
         num_zeroes = len(str(num))
         res = ''
         for digit in str(num):
-            char_to_add = ''
             num_zeroes -= 1
             if digit == '9':
-                char_to_add = zeroes[num_zeroes][1] + zeroes[num_zeroes+1][1]
+                res += zeroes[num_zeroes][1] + zeroes[num_zeroes+1][1]
             elif digit == '4':
-                char_to_add = zeroes[num_zeroes][1] + zeroes[num_zeroes][5]
+                res += zeroes[num_zeroes][1] + zeroes[num_zeroes][5]
             elif int(digit) >= 5:
-                char_to_add = zeroes[num_zeroes][5] + ''.join([zeroes[num_zeroes][1] for _ in range(int(digit) % 5)])
+                res += zeroes[num_zeroes][5] + ''.join([zeroes[num_zeroes][1] for _ in range(int(digit) % 5)])
             else:
-                char_to_add = ''.join([zeroes[num_zeroes][1] for _ in range(int(digit))])
-            res += char_to_add
+                res += ''.join([zeroes[num_zeroes][1] for _ in range(int(digit))])
         return res
