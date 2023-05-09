9ebf3e3720273ef689031172ebec2b8a9e21800a
fisjac <jfisher9882@gmail.com>
2023-05-09 13:34:22 -0500

    optimized remove duplicates

--- arrays/remove_duplicates_in_place.py
+++ arrays/remove_duplicates_in_place.py
@@ -1,14 +1,30 @@
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         k = len(nums)
+#         if k == 1: return k
+
+#         for i in range(len(nums)-1,0,-1):
+#             # print(i, nums)
+#             prev, current = nums[i-1],nums[i]
+#             if current == prev:
+#                 k -= 1
+#                 for j in range(i,k):
+#                     nums[j], nums[j+1] = nums[j+1], nums[j]
+#                     # print(nums, k)
+#         return k
+
+
+# Better Solution:
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        k = len(nums)
-        if k == 1: return k
+        left, right  = 1, 1
 
-        for i in range(len(nums)-1,0,-1):
-            # print(i, nums)
-            prev, current = nums[i-1],nums[i]
-            if current == prev:
-                k -= 1
-                for j in range(i,k):
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-                    # print(nums, k)
-        return k
+        while right < len(nums):
+            if nums[right] != nums[right - 1]:
+                nums[left] = nums[right]
+                left += 1
+                right += 1
+            else:
+                right += 1
+
+        return left
